884fbded79e48e2e001fea30e1ac6a9ae2048d4d
Piotr <websites@hello-24.com>
2022-07-24 16:56:55 -0500

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -150,14 +150,19 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const init = function () {
-  bookmarksView.addHandlerRender(controlBookmarks);
-  recipeView.addHandlerRender(controlRecipes);
-  recipeView.addHandlerUpdateServings(controlServings);
-  recipeView.addHandlerAddBookmark(controlAddBookmark);
-  searchView.addHandlerSearch(controlSearchResults);
-  paginationView.addHandlerClick(controlPagination);
-  addRecipeView.addHandlerUpload(controlAddRecipe);
+const newFeature = function () {
+  console.log('Welcome to the application!');
+
+  const init = function () {
+    bookmarksView.addHandlerRender(controlBookmarks);
+    recipeView.addHandlerRender(controlRecipes);
+    recipeView.addHandlerUpdateServings(controlServings);
+    recipeView.addHandlerAddBookmark(controlAddBookmark);
+    searchView.addHandlerSearch(controlSearchResults);
+    paginationView.addHandlerClick(controlPagination);
+    addRecipeView.addHandlerUpload(controlAddRecipe);
+    newFeature();
+  };
 };
 
 init();
