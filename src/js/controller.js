a35620949ec96402d1bd5a2d3c2f78ba1d507143
Piotr <websites@hello-24.com>
2022-07-25 12:47:23 -0500

    update code and delete NewFuture function

--- src/js/controller.js
+++ src/js/controller.js
@@ -150,19 +150,14 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-
-  const init = function () {
-    bookmarksView.addHandlerRender(controlBookmarks);
-    recipeView.addHandlerRender(controlRecipes);
-    recipeView.addHandlerUpdateServings(controlServings);
-    recipeView.addHandlerAddBookmark(controlAddBookmark);
-    searchView.addHandlerSearch(controlSearchResults);
-    paginationView.addHandlerClick(controlPagination);
-    addRecipeView.addHandlerUpload(controlAddRecipe);
-    newFeature();
-  };
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
